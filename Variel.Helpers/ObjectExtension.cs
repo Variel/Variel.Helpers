5978068de30727cb448cdc0371f9328e15437673
variel <blackmagic2@naver.com>
2015-02-21 23:50:14 +0900

    Object To String Dictionary Uri Query String Builder

---
+++ Variel.Helpers/ObjectExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variel.Helpers
+{
+    public static class ObjectExtension
+    {
+        public static Dictionary<string, string> ToStringDictionary(this object data)
+        {
+            const BindingFlags attr = BindingFlags.Public | BindingFlags.Instance;
+
+            return data.GetType()
+                       .GetProperties(attr)
+                       .Where(property => property.CanRead)
+                       .ToDictionary(
+                           property => property.Name,
+                           property => property.GetValue(data, null).ToString()
+                       );
+        }
+    }
+}
---
+++ Variel.Helpers/Web/UriStringBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variel.Helpers.Web
+{
+    public static class UriHelper
+    {
+        public static string Build(string uri, object query)
+        {
+            return Build(uri, query.ToStringDictionary());
+        }
+
+        public static string Build(string uri, IDictionary<string, string> query)
+        {
+            if (uri.Contains("?"))
+                throw new ArgumentException("uri는 Query String을 포함할 수 없습니다", "uri");
+
+            uri += "?" +
+                   String.Concat(query.Select(x => String.Format("{0}={1}&",
+                                                                 WebUtility.UrlEncode(x.Key),
+                                                                 WebUtility.UrlEncode(x.Value))));
+            return uri;
+        }
+    }
+}
